b01c708b03101a92781fb0730ed9187dc2fc711c
dnanonymous <adannbd@gmail.com>
2025-04-06 13:01:56 -0500

    Added submodule and committed changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Welcome to Payment S!</h1>
+        <p>Scroll down to see the button at the bottom.</p>
+      </div>
+      <button className="bottom-button">Click Me</button>
     </div>
   );
 }
 
 export default App;
+
+
